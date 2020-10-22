229b69812a1d0aa26adc21749ac1cd482c57e229
jetspiking <43465192+jetspiking@users.noreply.github.com>
2020-10-22 19:01:21 +0200

    Added Source Files

---
+++ HiddenLaunch/Classes/AppManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Management.Deployment;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace HiddenLaunch.Classes
+{
+    /**
+     * IMPORTANT! READ CAREFULLY!     
+     * 
+     * To use PackageManager you first need to make some changes in your appxmanifest. (Right click on Package.appxmanifest in the Solution Explorer and select View Code)
+     * These changes allow you to use restricted capabilities ("rescap" is the tag for this).
+     * 
+     * In package add "xmlns:rescap="http://schemas.microsoft.com/appx/manifest/foundation/windows10/restrictedcapabilities"" and under IgnorableNamespaces add "rescap".
+     * 
+     * Example (All adjusted properties should be inside the package opening tag!):
+     * 
+     * <Package  
+     * xmlns="http://schemas.microsoft.com/appx/manifest/foundation/windows10" 
+     * xmlns:mp="http://schemas.microsoft.com/appx/2014/phone/manifest" 
+     * xmlns:uap="http://schemas.microsoft.com/appx/manifest/uap/windows10" 
+     * xmlns:uap3="http://schemas.microsoft.com/appx/manifest/uap/windows10/3" 
+     * xmlns:rescap="http://schemas.microsoft.com/appx/manifest/foundation/windows10/restrictedcapabilities"
+     * IgnorableNamespaces="uap mp uap3 rescap">
+     *
+     * NOTE: "rescap" capabilities should be listed above all other capabilities! 
+     * 
+     * Now under the capabilities in your manifest add "rescap:Capability Name="packageManagement"" and "rescap:Capability Name="packageQuerys"", don't forget the opening and closing tag "<>".
+     *
+     * Example:
+     * 
+     * <Capabilities>
+     * <rescap:Capability Name="packageManagement"/>
+     * <rescap:Capability Name="packageQuerys" />
+     * </Capabilities>
+     * 
+     * NOTE: If your application uses restricted capabilities, you won't be able to upload to the Microsoft Store (so you must sideload your appx).
+     */
+
+    public static class AppManager
+    {
+        public static async void StartApp(AppListEntry appListEntry)
+        {
+            await appListEntry.LaunchAsync();
+        }
+
+        public static Image GetImageFromPackage(Package package)
+        {
+            List<AppListEntry> appListEntries = new List<AppListEntry>();
+            Task<IReadOnlyList<AppListEntry>> getAppEntriesTask = package.GetAppListEntriesAsync().AsTask();
+            getAppEntriesTask.Wait();
+            appListEntries = getAppEntriesTask.Result.ToList();
+
+            return GetImageFromEntry(appListEntries[0]);
+        }
+
+        public static Image GetImageFromEntry(AppListEntry appListEntry)
+        {
+            BitmapImage logo = new BitmapImage();
+            var logoStream = appListEntry.DisplayInfo.GetLogo(new Size(50, 50));
+            Task<IRandomAccessStreamWithContentType> logoStreamTask = logoStream.OpenReadAsync().AsTask();
+            logoStreamTask.Wait();
+            IRandomAccessStreamWithContentType logoStreamResult = logoStreamTask.Result;
+            logo.SetSource(logoStreamResult);
+
+            Image image = new Image();
+            image.Source = logo;
+
+            return image;
+        }
+
+            public static List<Package> GetAppPackages()
+        {
+            PackageManager packageManager = new PackageManager();
+            IEnumerable<Package> packageEnumerable = packageManager.FindPackagesForUser(String.Empty);          // UWP apps can't run without administrator permissions, this is why you need to call "FindPackagesForUser("")" instead of "FindPackages()" Administrator permissions are mandatory when going outside the current user.
+            return packageEnumerable.ToList();
+        }
+
+        public static async Task<List<AppListEntry>> GetAppEntries()
+        {
+            PackageManager packageManager = new PackageManager();
+            IEnumerable<Package> packageEnumerable = packageManager.FindPackagesForUser(String.Empty);          // UWP apps can't run without administrator permissions, this is why you need to call "FindPackagesForUser("")" instead of "FindPackages()" Administrator permissions are mandatory when going outside the current user.
+            List<AppListEntry> appListEntries = new List<AppListEntry>();
+
+            foreach (Package package in packageEnumerable)
+            {
+                try
+                {
+                    IReadOnlyList<AppListEntry> apps = await package.GetAppListEntriesAsync();
+                    appListEntries.AddRange(apps);
+                }
+                catch (System.Exception) {};                                                                   // On Windows 10 Mobile devices this try-catch is needed to prevent an error, I don't know why it is thrown.
+            }
+
+            return appListEntries;
+        }
+    }
+}
---
+++ HiddenLaunch/MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using HiddenLaunch.Classes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Management.Deployment;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace HiddenLaunch
+{
+    public sealed partial class MainPage : Page, IDialogCallback
+    {
+        List<AppListEntry> applicationList = new List<AppListEntry>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            AppDesign.SetStatusBarColor((Color)this.Resources[PropertyString.SYSTEM_ACCENT_COLOR], Colors.Black);
+            InitializeAppList();
+            AppList.SelectionChanged += AppListViewEvent;
+        }
+
+        async void InitializeAppList()
+        {
+            IReadOnlyList<AppListEntry> appListEntryList;
+            List<Package> packages = AppManager.GetAppPackages();
+
+            foreach (Package package in packages)
+            {
+                try
+                {
+                    appListEntryList = await package.GetAppListEntriesAsync();
+
+                    foreach (AppListEntry appListEntry in appListEntryList)
+                    {
+                        if (appListEntry == null) continue;
+
+                        TextBlock appName = new TextBlock();
+                        appName.Text = appListEntry.DisplayInfo.DisplayName;
+
+                        TextBlock appDescription = new TextBlock();
+                        appDescription.Text = appListEntry.DisplayInfo.Description;
+
+                        Image appIcon = AppManager.GetImageFromEntry(appListEntry);
+
+                        Size screenSize = Tool.GetScreenSize();
+
+                        StackPanel stackPanel = new StackPanel();
+                        stackPanel.Orientation = Orientation.Horizontal;
+                        stackPanel.Width = screenSize.Width;
+
+                        appIcon.Width = screenSize.Width / 5;
+                        appIcon.Height = appIcon.Width;
+                        stackPanel.Height = appIcon.Height;
+
+                        stackPanel.Children.Add(appIcon);
+                        stackPanel.Children.Add(appName);
+                        stackPanel.Margin = new Thickness(0,0,0,5);
+
+                        appName.Margin = new Thickness(appIcon.Width / 2, 0, 0, 0);
+
+                        AppList.Items.Add(stackPanel);
+
+                        this.applicationList.Add(appListEntry);
+                    }
+                }
+                catch (System.Exception) { };
+            }
+        }
+
+        private void AppListViewEvent(object sender, SelectionChangedEventArgs e)
+        {
+            ListView listView = sender as ListView;
+            if (listView.SelectedIndex < 0 || listView.SelectedIndex > applicationList.Count) return;
+            CustomDialog customDialog = new CustomDialog(this, "Launch Application", applicationList[listView.SelectedIndex].DisplayInfo.DisplayName, "Ok", "Cancel");
+            customDialog.ShowDialog(applicationList[listView.SelectedIndex]);
+        }
+
+
+        public async void NotifyFromDialog(ContentDialogResult dialogResult, object argument)
+        {
+            if (dialogResult == ContentDialogResult.Primary)
+            {
+                AppListEntry app = argument as AppListEntry;
+                await app.LaunchAsync();
+            }
+        }
+    }
+
+}
+
